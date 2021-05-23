3e75edec49342c342537b7044e0fa71cf8f049a0
CarinaAlessandro <71675000+CarinaAlessandro@users.noreply.github.com>
2021-05-23 18:30:00 -0400

    here it is before i mess it up

--- src/js/index.js
+++ src/js/index.js
@@ -2,3 +2,56 @@ import { DOMSelectors } from "./DOM";
 import { genres } from "./genre";
 
 const key = "YOURKEYHERE";
+const query = async function () {
+  try {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/discover/movie?api_key=${key}&language=en-US&sort_by=vote_average.desc&include_adult=false&include_video=false&page=1&vote_count.gte=4000&vote_average.gte=8&with_watch_monetization_types=flatrate`
+    );
+    const data = await response.json();
+    data.results.forEach((movie) => {
+      let genreArr = [];
+      const addGenre = function () {
+        genres.forEach((element) => {
+          if (movie.genre_ids.includes(element.id)) {
+            genreArr.push(element.name);
+            return genreArr;
+          }
+        });
+      };
+      addGenre();
+      console.log(genreArr);
+      DOMSelectors.grid.insertAdjacentHTML(
+        "beforeend",
+        `<div class="movie-card">
+            <div class="movie-card-front">
+              <img
+                src="https://image.tmdb.org/t/p/w300/${movie.poster_path}"
+                alt=""
+                class="poster"
+              />
+            </div>
+            <div class="movie-card-back">
+              <h3 class="movie-card-header">${movie.original_title}</h3>
+              <div class="score-box">
+                <p class="user-score">Community Score</p>
+                <p class="user-score">${movie.vote_average}</p>
+              </div>
+    
+              <div class="release-box">
+                <p class="release-date">Released</p>
+                <p class="release-date">${movie.release_date}</p>
+              </div>
+    
+              <div class="movie-genres">
+                 ${genreArr}
+              </div>
+            </div>
+          </div>`
+      );
+    });
+  } catch (error) {
+    console.log(error);
+    alert("Something went wrong.");
+  }
+};
+query();
